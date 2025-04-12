d886587a32c9390de5324e46bdfc6783677c971c
Ritik8097 <ritikpatelrp786@gmail.com>
2025-04-12 11:46:30 +0530

    mn

---
+++ services/script.js
@@ -0,0 +1,21 @@
+const lenis = new Lenis();
+
+function raf(time){
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+const parallaxItems = document.querySelectorAll('[data-speed]');
+
+  window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+
+    parallaxItems.forEach(item => {
+      const speed = parseFloat(item.dataset.speed);
+      const offsetTop = item.offsetTop;
+      const translateY = (scrollY - offsetTop) * speed;
+
+      item.style.transform = `translateY(${translateY}px)`;
+    });
+  });
